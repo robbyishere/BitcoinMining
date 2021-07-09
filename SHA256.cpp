49576a01163d683afa2d1b6ca8c824ca9eab3238
robbyishere <jkoh1295@gmail.com>
2021-07-09 12:04:31 -0700

    Replaced ConstantValues and HashValues code with shortcut

--- SHA256.cpp
+++ SHA256.cpp
@@ -1,5 +1,4 @@
 /* TODO:
-Make ConstantValues and HashValues shortcut
 Cut down struct array values?
 Improve binaryAdditionFunction
 */
@@ -23,12 +22,10 @@ struct equationShiftValues{
 	int Value[3];
 } Shift[4];
 struct ConstantValues{
-	double Original;
-	string Computed;
-	int WorkingValues[32];
+	string WorkingValues;
 }ConstantValues[64];
 struct HashValues{
-	double InitialValues;
+	string InitialValues;
 	int OriginalValues[32];
 	int WorkingValues[32];
 }HashValues[8];
@@ -51,192 +48,94 @@ void populateShiftValues(){
 	Shift[3].Value[2] = 25;
 }
 void computeConstants(){
-	ConstantValues[0].Original = cbrt(2);
-	ConstantValues[1].Original = cbrt(3);
-	ConstantValues[2].Original = cbrt(5);
-	ConstantValues[3].Original = cbrt(7);
-	ConstantValues[4].Original = cbrt(11); 
-	ConstantValues[5].Original = cbrt(13);
-	ConstantValues[6].Original = cbrt(17);
-	ConstantValues[7].Original = cbrt(19);
-	ConstantValues[8].Original = cbrt(23);
-	ConstantValues[9].Original = cbrt(29);
-	ConstantValues[10].Original = cbrt(31);
-	ConstantValues[11].Original = cbrt(37);
-	ConstantValues[12].Original = cbrt(41);
-	ConstantValues[13].Original = cbrt(43);
-	ConstantValues[14].Original = cbrt(47);
-	ConstantValues[15].Original = cbrt(53);
-	ConstantValues[16].Original = cbrt(59);
-	ConstantValues[17].Original = cbrt(61);
-	ConstantValues[18].Original = cbrt(67);
-	ConstantValues[19].Original = cbrt(71);
-	ConstantValues[20].Original = cbrt(73);
-	ConstantValues[21].Original = cbrt(79);
-	ConstantValues[22].Original = cbrt(83);
-	ConstantValues[23].Original = cbrt(89);
-	ConstantValues[24].Original = cbrt(97);
-	ConstantValues[25].Original = cbrt(101);
-	ConstantValues[26].Original = cbrt(103);
-	ConstantValues[27].Original = cbrt(107);
-	ConstantValues[28].Original = cbrt(109);
-	ConstantValues[29].Original = cbrt(113);
-	ConstantValues[30].Original = cbrt(127);
-	ConstantValues[31].Original = cbrt(131);
-	ConstantValues[32].Original = cbrt(137);
-	ConstantValues[33].Original = cbrt(139);
-	ConstantValues[34].Original = cbrt(149);
-	ConstantValues[35].Original = cbrt(151);
-	ConstantValues[36].Original = cbrt(157);
-	ConstantValues[37].Original = cbrt(163);
-	ConstantValues[38].Original = cbrt(167);
-	ConstantValues[39].Original = cbrt(173);
-	ConstantValues[40].Original = cbrt(179);
-	ConstantValues[41].Original = cbrt(181);
-	ConstantValues[42].Original = cbrt(191);
-	ConstantValues[43].Original = cbrt(193);
-	ConstantValues[44].Original = cbrt(197);
-	ConstantValues[45].Original = cbrt(199);
-	ConstantValues[46].Original = cbrt(211);
-	ConstantValues[47].Original = cbrt(223);
-	ConstantValues[48].Original = cbrt(227);
-	ConstantValues[49].Original = cbrt(229);
-	ConstantValues[50].Original = cbrt(233);
-	ConstantValues[51].Original = cbrt(239);
-	ConstantValues[52].Original = cbrt(241);
-	ConstantValues[53].Original = cbrt(251);
-	ConstantValues[54].Original = cbrt(257);
-	ConstantValues[55].Original = cbrt(263);
-	ConstantValues[56].Original = cbrt(269);
-	ConstantValues[57].Original = cbrt(271);
-	ConstantValues[58].Original = cbrt(277);
-	ConstantValues[59].Original = cbrt(281);
-	ConstantValues[60].Original = cbrt(283);
-	ConstantValues[61].Original = cbrt(293);
-	ConstantValues[62].Original = cbrt(307);
-	ConstantValues[63].Original = cbrt(311);
-	for(int i=0; i<64; i++){
-		int ConstantInteger = ConstantValues[i].Original;
-		double hex = (ConstantValues[i].Original - ConstantInteger); //Remove starting integer 
-		cout.precision(16);
-		int HexStore[8];
-		char input[8];
-		//Convert to hex
-		for(int j=0; j<8; j++){
-			hex = hex*16;
-			HexStore[j] = hex;
-			hex = hex - HexStore[j];
-			//Convert to binary
-			switch(HexStore[j]){
-				case 0:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0000";
-					break;
-				case 1:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0001";
-					break;
-				case 2:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0010";
-					break;
-				case 3:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0011";
-					break;
-				case 4:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0100";
-					break;
-				case 5:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0101";
-					break;
-				case 6:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0110";
-					break;
-				case 7:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "0111";
-					break;
-				case 8:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1000";
-					break;
-				case 9:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1001";
-					break;
-				case 10:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1010";
-					break;
-				case 11:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1011";
-					break;
-				case 12:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1100";
-					break;
-				case 13:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1101";
-					break;
-				case 14:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1110";
-					break;
-				case 15:
-					ConstantValues[i].Computed = ConstantValues[i].Computed + "1111";
-					break;
-			}
-		}
-	//Convert string to int array
-	for(int j=0; j<32; j++){
-		if (ConstantValues[i].Computed[j] == '1'){
-			ConstantValues[i].WorkingValues[j] = 1;
-		}
-		else{
-			ConstantValues[i].WorkingValues[j] = 0;
-		}
-	}
-	}
+	ConstantValues[0].WorkingValues = "01000010100010100010111110011000";
+	ConstantValues[1].WorkingValues = "01110001001101110100010010010001";
+	ConstantValues[2].WorkingValues = "10110101110000001111101111001111";
+	ConstantValues[3].WorkingValues = "11101001101101011101101110100101";
+	ConstantValues[4].WorkingValues = "00111001010101101100001001011011"; 
+	ConstantValues[5].WorkingValues = "01011001111100010001000111110001";
+	ConstantValues[6].WorkingValues = "10010010001111111000001010100100";
+	ConstantValues[7].WorkingValues = "10101011000111000101111011010101";
+	ConstantValues[8].WorkingValues = "11011000000001111010101010011000";
+	ConstantValues[9].WorkingValues = "00010010100000110101101100000001";
+	ConstantValues[10].WorkingValues = "00100100001100011000010110111110";
+	ConstantValues[11].WorkingValues = "01010101000011000111110111000011";
+	ConstantValues[12].WorkingValues = "01110010101111100101110101110100";
+	ConstantValues[13].WorkingValues = "10000000110111101011000111111110";
+	ConstantValues[14].WorkingValues = "10011011110111000000011010100111";
+	ConstantValues[15].WorkingValues = "11000001100110111111000101110100";
+	ConstantValues[16].WorkingValues = "11100100100110110110100111000001";
+	ConstantValues[17].WorkingValues = "11101111101111100100011110000110";
+	ConstantValues[18].WorkingValues = "00001111110000011001110111000110";
+	ConstantValues[19].WorkingValues = "00100100000011001010000111001100";
+	ConstantValues[20].WorkingValues = "00101101111010010010110001101111";
+	ConstantValues[21].WorkingValues = "01001010011101001000010010101010";
+	ConstantValues[22].WorkingValues = "01011100101100001010100111011100";
+	ConstantValues[23].WorkingValues = "01110110111110011000100011011010";
+	ConstantValues[24].WorkingValues = "10011000001111100101000101010010";
+	ConstantValues[25].WorkingValues = "10101000001100011100011001101101";
+	ConstantValues[26].WorkingValues = "10110000000000110010011111001000";
+	ConstantValues[27].WorkingValues = "10111111010110010111111111000111";
+	ConstantValues[28].WorkingValues = "11000110111000000000101111110011";
+	ConstantValues[29].WorkingValues = "11010101101001111001000101000111";
+	ConstantValues[30].WorkingValues = "00000110110010100110001101010001";
+	ConstantValues[31].WorkingValues = "00010100001010010010100101100111";
+	ConstantValues[32].WorkingValues = "00100111101101110000101010000101";
+	ConstantValues[33].WorkingValues = "00101110000110110010000100111000";
+	ConstantValues[34].WorkingValues = "01001101001011000110110111111100";
+	ConstantValues[35].WorkingValues = "01010011001110000000110100010011";
+	ConstantValues[36].WorkingValues = "01100101000010100111001101010100";
+	ConstantValues[37].WorkingValues = "01110110011010100000101010111011";
+	ConstantValues[38].WorkingValues = "10000001110000101100100100101110";
+	ConstantValues[39].WorkingValues = "10010010011100100010110010000101";
+	ConstantValues[40].WorkingValues = "10100010101111111110100010100001";
+	ConstantValues[41].WorkingValues = "10101000000110100110011001001011";
+	ConstantValues[42].WorkingValues = "11000010010010111000101101110000";
+	ConstantValues[43].WorkingValues = "11000111011011000101000110100011";
+	ConstantValues[44].WorkingValues = "11010001100100101110100000011001";
+	ConstantValues[45].WorkingValues = "11010110100110010000011000100100";
+	ConstantValues[46].WorkingValues = "11110100000011100011010110000101";
+	ConstantValues[47].WorkingValues = "00010000011010101010000001110000";
+	ConstantValues[48].WorkingValues = "00011001101001001100000100010110";
+	ConstantValues[49].WorkingValues = "00011110001101110110110000001000";
+	ConstantValues[50].WorkingValues = "00100111010010000111011101001100";
+	ConstantValues[51].WorkingValues = "00110100101100001011110010110101";
+	ConstantValues[52].WorkingValues = "00111001000111000000110010110011";
+	ConstantValues[53].WorkingValues = "01001110110110001010101001001010";
+	ConstantValues[54].WorkingValues = "01011011100111001100101001001111";
+	ConstantValues[55].WorkingValues = "01101000001011100110111111110011";
+	ConstantValues[56].WorkingValues = "01110100100011111000001011101110";
+	ConstantValues[57].WorkingValues = "01111000101001010110001101101111";
+	ConstantValues[58].WorkingValues = "10000100110010000111100000010100";
+	ConstantValues[59].WorkingValues = "10001100110001110000001000001000";
+	ConstantValues[60].WorkingValues = "10010000101111101111111111111010";
+	ConstantValues[61].WorkingValues = "10100100010100000110110011101011";
+	ConstantValues[62].WorkingValues = "10111110111110011010001111110111";
+	ConstantValues[63].WorkingValues = "11000110011100010111100011110010";
 }
 void computeInitialHashValues(){
-	HashValues[0].InitialValues = sqrt(2);
-	HashValues[1].InitialValues = sqrt(3);
-	HashValues[2].InitialValues = sqrt(5);
-	HashValues[3].InitialValues = sqrt(7);
-	HashValues[4].InitialValues = sqrt(11);
-	HashValues[5].InitialValues = sqrt(13);
-	HashValues[6].InitialValues = sqrt(17);
-	HashValues[7].InitialValues = sqrt(19);
-	int InitialInteger;
+	HashValues[0].InitialValues = "01101010000010011110011001100111";
+	HashValues[1].InitialValues = "10111011011001111010111010000101";
+	HashValues[2].InitialValues = "00111100011011101111001101110010";
+	HashValues[3].InitialValues = "10100101010011111111010100111010";
+	HashValues[4].InitialValues = "01010001000011100101001001111111";
+	HashValues[5].InitialValues = "10011011000001010110100010001100";
+	HashValues[6].InitialValues = "00011111100000111101100110101011";
+	HashValues[7].InitialValues = "01011011111000001100110100011001";
+	//Duplicate values
 	for(int i=0; i<8; i++){
-		InitialInteger = HashValues[i].InitialValues;
-		int binary[32];
-		//remove starting integer and multiply by 2^32
-		double binaryConvert = trunc(((HashValues[i].InitialValues - InitialInteger) * pow(2,32)));
-		int loop = 0;
-		//convert to binary
-		while(binaryConvert>0){
-			if(fmod(binaryConvert,2) >= 1){ //Divide by 2 and see if there is a remainder. If yes, bit is 1
-				binary[loop] = 1;
-			}
-			else{
-				binary[loop] = 0;
-			}
-			binaryConvert = trunc(binaryConvert); //remove decimal if there is one
-			binaryConvert = binaryConvert/2; //prepare for calculating next bit
-			loop++;
-		}
-		//Pad to 32 bits
-		for(int j=loop; j<32; j++){
-			binary[j] = 0;
-		}
-		//Reverse and Vonvert to int array
 		for(int j=0; j<32; j++){
-			if(binary[31-j] == 1){
+			if(HashValues[i].InitialValues[j] == '1'){
+				HashValues[i].OriginalValues[j] = 1;
 				HashValues[i].WorkingValues[j] = 1;
 			}
-			else{
+			if(HashValues[i].InitialValues[j] == '0'){
+				HashValues[i].OriginalValues[j] = 0;
 				HashValues[i].WorkingValues[j] = 0;
 			}
 		}
-		//Duplicate values
-		for(int j=0; j<32; j++){
-			HashValues[i].OriginalValues[j] = HashValues[i].WorkingValues[j];
-		}
-		}
-
 	}
+}
 string binaryToHex(string output[]){
 	string result;
 	for(int i=0; i<64; i++){
@@ -349,9 +248,11 @@ string hexToBinary(char input[], int characterCount){
 }
 
 string SHA256(char minerInput[]){
+	populateShiftValues();
+	computeConstants();
 	string result;
 	for(int i=0; i<2; i++){ //Loops twice to compute SHA256 twice for correct block hash
-		
+		computeInitialHashValues(); //Needs to be recalculated every hash
 		//Convert input to binary
 		string message = hexToBinary(minerInput, 160);
 		
@@ -390,9 +291,6 @@ string SHA256(char minerInput[]){
 		if(i==1){
 			blockCount = 1;
 		}
-		populateShiftValues();
-		computeConstants();
-		computeInitialHashValues();
 		
 		//Loop for every block
 		for(int j=0; j<blockCount; j++){
@@ -444,7 +342,12 @@ string SHA256(char minerInput[]){
 				for(int m=0; m<32; m++){
 					binaryAddition[0].Data[m] = Data[3].result[m];
 					binaryAddition[2].Data[m] = HashValues[7].WorkingValues[m];
-					binaryAddition[3].Data[m] = ConstantValues[k].WorkingValues[m];
+					if(ConstantValues[k].WorkingValues[m] == '1'){
+						binaryAddition[3].Data[m] = 1;
+					}
+					if(ConstantValues[k].WorkingValues[m] == '0'){
+						binaryAddition[3].Data[m] = 0;
+					}
 					binaryAddition[4].Data[m] = schedule[j][k].word[m];
 				}
 				choice();
