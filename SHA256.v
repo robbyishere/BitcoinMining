bea432db735b393e36d4f59b64858c4dd1ce05d5
robbyishere <jkoh1295@gmail.com>
2021-07-21 15:10:44 -0700

    Initialized message schedule

--- SHA256.v
+++ SHA256.v
@@ -1,19 +1,19 @@
 /*
 TODO:
 	Learn how always blocks work
-	How to work with arrays
 	How to call functions
+	Make Right Shift wrap
 */
 module hash;
 
 reg [0:1023] ins;
-reg [0:31] ins_Split [0:64];
+reg [0:31] ins_Split [0:63];
 reg [0:1] blockCount;
-reg [31:0] binaryAddition [4:0];
-integer i, j, k, m, temp;
+reg [0:31] binaryAddition [0:4];
+integer i, j, k, m;
 
 initial begin
-    assign ins[0:607] = 608'h0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b;
+    ins[0:607] = 608'h0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b;
     for(i=0; i<2; i=i+1) begin
 		$display("");
 		$display("Hash Step: ", i);
@@ -25,26 +25,32 @@ initial begin
 			ins[1016] = 1'b1;
 			blockCount = 2;
 			$display("%b", ins);
-			ins[0:1023] = 1024'b0;
 		end
 		if(i == 1) begin
-			ins[256:511] = 416'b1;
+			ins[256:511] = 416'b0;
 			ins[256] = 1'b1;
 			ins[503] = 1'b1;
 			blockCount = 1;
-			$display("%b", ins);
-			ins[0:1023] = 1024'b0;
-		end
-		//Initialize message schedule
-		for(j=0; j<64; j=j+1)begin
+			$display("%b", ins[0:511]);
 		end
 		for(j=0; j<blockCount; j=j+1)begin
 			$display("Block: ", j);
-			for(k=16; k<64; k=k+1)begin
-				//temp = (9*32)+((k-16)*32);
-				//binaryAddition[1] = ins[temp:temp+32];
-				//$display("binaryAddition[1]: %b", binaryAddition[1]);
+
+			//Split block into 16, 32 bit words
+			for(k=0; k<16; k=k+1)begin
+				ins_Split[k] = ins[32*k+:32];
+				$display("%b", ins_Split[k]);
 			end
+			
+			$display(" ");
+			//Fill out rest of message schedule
+			for(k=16; k<64; k=k+1)begin
+				binaryAddition[1] = ins_Split[k-7];
+				binaryAddition[3] = ins_Split[k-16];
+				//$display("%b", binaryAddition[3]);
+			end
+			
+			
 		end
 	end
 end
